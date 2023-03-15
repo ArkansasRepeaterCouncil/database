928f8ddd96e1029cec39bfc9b5f5cacdf5a0a39f
Joshua Carroll <joshcarroll21@gmail.com>
2023-03-15 15:52:31 -0500

    Deleted

--- sp_MetersToMiles.sql
+++
@@ -1,5 +0,0 @@
-CREATE PROCEDURE sp_MetersToMiles @meters decimal(18,2), @miles decimal(18,2) output
-AS
-BEGIN
-	SET @miles = @meters / 1609.34;
-END
--- sp_MilesToMeters.sql
+++
@@ -1,5 +0,0 @@
-CREATE PROCEDURE sp_MilesToMeters @miles decimal(18,2), @meters decimal(18,2) output
-AS
-BEGIN
-	SET @meters = @miles * 1609.34;
-END
